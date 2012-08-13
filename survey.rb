711c4d282f67a16c5aa74ef82973c5c581f3f6f4
Chris Riddoch <riddochc@gmail.com>
2012-08-12 18:36:54 -0600

    Abstracted away layout for each question type to specific files
    Implementing basic presentation of questions, processing
    posts of answers properly redirects to next question or done page.
    
    Simple template: Outer page handled by layout.erb, specific question
    types handled by more specific files.
    
    Signed-off-by: Chris Riddoch <riddochc@gmail.com>

--- survey.rb
+++ survey.rb
@@ -5,6 +5,9 @@ require 'erb'
 require 'sequel'
 require 'sinatra/cookies'
 
+class NoSuchQuestion < Exception
+end
+
 #set :server, :thin
 DB = Sequel.connect('sqlite://survey.sqlite3')
 enable :sessions
@@ -28,16 +31,47 @@ end
 
 get %r{^/step/(\d+)} do |i|
   step = i.to_i
-  erb :step, :locals => {:s => step}
+  question = DB[:questions][:id => step]
+  if question.nil?
+    raise NoSuchQuestion, step
+  end
+  case question[:question_type_id]
+  when 1
+    erb :rank, :locals => {:q => question, :s => step}
+  when 2
+    erb :multiple_selection, :locals => {:q => question, :s => step}
+  when 3
+    erb :single_selection_or_text_entry, :locals => {:q => question, :s => step}
+  else
+    "Oops."
+  end
+  # erb :step, :locals => {:s => step}
 end
 
 post %r{^/step/(\d+)} do |i|
-  # Do some processing...
-  puts "Processing step #{i}"
-  next_step = i.to_i.succ
-  next_path = "/step/#{next_step}"
-  redirect next_path, 302
+  step = i.to_i
+  question = DB[:questions][:id => step]
+  if question.nil?
+    raise NoSuchQuestion, step
+  end
+  number_of_questions = DB[:questions].count
+  if step > number_of_questions
+    raise NoSuchQuestion, step
+  end
+
+  # Process answer for question
+  
+  if (step < number_of_questions)
+    next_step = i.to_i.succ
+    next_path = "/step/#{next_step}"
+    redirect next_path, 302
+  elsif (number_of_questions == step)
+    redirect "/done", 302
+  else
+    "Hmm. Not sure what to do here."
+  end
 end
 
-
-
+get "/done" do
+  erb :done
+end
